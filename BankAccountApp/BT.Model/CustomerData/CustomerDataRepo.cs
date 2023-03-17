11ce4f67ad409e13056a344601c9d8beb3fe9bd1
Jesse <Jesse.Quale@hotmail.com>
2023-03-17 12:54:28 -0700

    Stub out methods in Customer repo

---
+++ BankAccountApp/BT.Model/CustomerData/CustomerDataRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BT.Model.CustomerData
+{
+	public class CustomerDataRepo : ICustomerDataRepository
+	{
+		//private readonly 
+		public void DeleteCustomer(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerable<Customer> ICustomerDataRepository.GetCustomers(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		void ICustomerDataRepository.InsertCustomer(Customer customer)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ BankAccountApp/BT.Model/CustomerData/ICustomerDataRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BT.Model.CustomerData
+{
+	internal interface ICustomerDataRepository
+	{
+		IEnumerable<Customer> GetCustomers(int id);
+		void InsertCustomer(Customer customer);
+		void DeleteCustomer(int id);	
+	}
+}
