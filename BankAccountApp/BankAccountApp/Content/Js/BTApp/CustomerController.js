b244decc6454eca75e58921e93ddd6b011ac518a
Jesse <Jesse.Quale@hotmail.com>
2023-04-10 13:03:21 -0700

    Customer input validation

--- BankAccountApp/BankAccountApp/Content/Js/BTApp/CustomerController.js
+++ BankAccountApp/BankAccountApp/Content/Js/BTApp/CustomerController.js
@@ -1,5 +1,11 @@
 ﻿console.log("Hit Customer Controller");
 if (typeof BT === 'undefined') { BT = {}; }
 BT.App.controller('CustomerController', function ($scope) {
-        
+    $scope.clearCustomerInput = function () {
+        $scope.form = null;
+        console.log("clar i[nput");
+    };
+    $scope.AddCustomer = function () {
+        console.log("cust add ... " + $scope.form.fname);
+    };      
 });
